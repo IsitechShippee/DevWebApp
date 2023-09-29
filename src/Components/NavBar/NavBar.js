2705f96d66b3389cafcc26beea11c0901096c950
TristanIsitech <tristan.labaume@ecole-isitech.fr>
2023-09-29 14:24:10 +0200

    Patch

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -21,15 +21,15 @@ function NavBar(props) {
         <h1>Rechercher</h1>
       </Link>
       <Link to='/newpost' onClick={() => acitveElement(2)} className={props.isActive === 2 ? 'active' : ''}>
-        <SVG.Home />
+        <SVG.Add />
         <h1>NewPost</h1>
       </Link>
       <Link to='/chat' onClick={() => acitveElement(3)} className={props.isActive === 3 ? 'active' : ''}>
-        <SVG.Home />
+        <SVG.Chat />
         <h1>Message</h1>
       </Link>
       <Link to='/user' onClick={() => acitveElement(4)} className={props.isActive === 4 ? 'user active' : 'user'}>
-        <SVG.Home />
+        <SVG.User />
         <h1>Compte</h1>
       </Link>
     </div>
--- src/SVG/SVG.js
+++ src/SVG/SVG.js
@@ -1,15 +1,15 @@
-import {ReactComponent as Home} from './Home.svg'
-// import {ReactComponent as Add} from './Ajout.svg'
-// import {ReactComponent as Search} from './Search.svg'
-// import {ReactComponent as User} from './User.svg'
-// import {ReactComponent as Edit} from './Edit.svg'
+import {ReactComponent as Home} from './HomeSVG.svg'
+import {ReactComponent as Add} from './Ajout.svg'
+import {ReactComponent as Search} from './Search.svg'
+import {ReactComponent as User} from './User.svg'
+import {ReactComponent as Chat} from './Chat.svg'
 
 const SVG = {
     Home: Home,
-    // Add: Add,
-    // Search: Search,
-    // User: User,
-    // Edit: Edit
+    Add: Add,
+    Search: Search,
+    User: User,
+    Chat: Chat
 }
 
 export default SVG
